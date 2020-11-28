45a314259090db6768130416d09d1d890bef2b67
veritaspoutine <14975224+veritaspoutine@users.noreply.github.com>
2020-11-28 14:56:35 +0800

    added code for Generate top 10 movie recommendations for a specified set of users

--- recommender/recommender_als.py
+++ recommender/recommender_als.py
@@ -3,6 +3,7 @@ from pyspark.sql.functions import *
 from pyspark.ml.feature import VectorAssembler,VectorIndexer,OneHotEncoder,StringIndexer
 from pyspark.ml.recommendation import ALS
 from pyspark.ml.evaluation import RegressionEvaluator
+import time
 
 
 if __name__ == "__main__":
@@ -20,16 +21,19 @@ if __name__ == "__main__":
     train_data, test_data = final_data.randomSplit([0.8,0.2])
     
     # Model training
-    als = ALS(maxIter=5,userCol="userId",itemCol="movieId",ratingCol="rating" , coldStartStrategy="drop")
+    als = ALS(maxIter=10,userCol="userId",itemCol="movieId",ratingCol="rating" , coldStartStrategy="drop")
+    start=time.time()
     model = als.fit(train_data)
     
     # Transform the test data using the model to get predictions
     predicted_test_data = model.transform(test_data)
-    model.write().overwrite().save("hdfs://master.tibame/user/clubs/spark_mllib_101/movies/movies_recommender/")
+    #model.write().overwrite().save("hdfs://master.tibame/user/clubs/spark_mllib_101/movies/movies_recommender/")
 
     # Evalute model performance with test set
     evaluator = RegressionEvaluator(predictionCol="prediction", labelCol="rating", metricName="rmse")
     print("rmse: {}".format(evaluator.evaluate(predicted_test_data)))
+    stop=time.time()
+    print(stop-start)
 
     # Specify the number of movies you would like to recommand for each user
     user_movies = model.recommendForAllUsers(3)
